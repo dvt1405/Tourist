fac8e6a4b9f80c15a5144cb1860dc58a4ba752e3
tuandv4 <dvt1405@gmail.com>
2024-05-23 11:54:57 +0700

    Update config language

--- app/src/main/java/com/kt/apps/media/taipeitour/Constants.kt
+++ app/src/main/java/com/kt/apps/media/taipeitour/Constants.kt
@@ -1,7 +1,20 @@
 package com.kt.apps.media.taipeitour
 
+import java.util.Locale
+
 object Constants {
     const val BASE_URL = "https://www.travel.taipei/"
 
     val supportedLanguages = listOf("zh-tw", "zh-cn", "en", "ja", "ko", "es", "id", "th", "vi")
+    val supportedLanguagesCode = mapOf(
+        "zh-tw" to Locale.TAIWAN.language,
+        "zh-cn" to Locale.CHINESE.language,
+        "en" to Locale.ENGLISH.language,
+        "ja" to Locale.JAPAN.language,
+        "ko" to Locale.KOREA.language,
+        "es" to "es",
+        "id" to "in",
+        "th" to "th",
+        "vi" to "vi"
+    )
 }
--- app/src/main/java/com/kt/apps/media/taipeitour/base/BaseActivity.kt
+++ app/src/main/java/com/kt/apps/media/taipeitour/base/BaseActivity.kt
@@ -1,7 +1,7 @@
 package com.kt.apps.media.taipeitour.base
 
+import android.content.res.Configuration
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,16 +9,24 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.kt.apps.media.taipeitour.Constants
+import com.kt.apps.media.taipeitour.data.settings.LanguageSettings
+import java.util.Locale
+import javax.inject.Inject
+
 
 abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
     abstract val layoutRes: Int
 
     protected lateinit var binding: T
 
+    @Inject
+    lateinit var languageSettings: LanguageSettings
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        updateLocal()
         enableEdgeToEdge()
-        Log.d("BaseActivity", "onCreate: $savedInstanceState")
         binding = DataBindingUtil.setContentView(this, layoutRes)
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -29,7 +37,19 @@ abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
         initAction(savedInstanceState)
     }
 
+    private fun updateLocal() {
+        val languageCode = Constants.supportedLanguagesCode[languageSettings.getLanguage()]
+            ?: languageSettings.getLanguage()
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.locale = locale
+        baseContext.resources.updateConfiguration(
+            config,
+            baseContext.resources.displayMetrics
+        )
+    }
+
     abstract fun initView(savedInstanceState: Bundle?)
     abstract fun initAction(savedInstanceState: Bundle?)
-
 }
